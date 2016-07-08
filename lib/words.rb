3f40e81f82e6d167ec0425b59ab4c52bd93565d7
Tony Fabery <tfabery@gmail.com>
2016-07-07 23:42:21 -0700

    save a Word object into @@words class variable

--- lib/words.rb
+++ lib/words.rb
@@ -1,8 +1,22 @@
 class Word
+  @@words = []
+
   attr_reader(:word, :definition)
 
   define_method(:initialize) do |word|
     @word = word
     @definition = []
+  end
+
+  define_singleton_method(:all) do
+    @@words
+  end
+
+  define_method(:save) do
+    @@words.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@words = []
   end
 end
--- spec/words_spec.rb
+++ spec/words_spec.rb
@@ -3,10 +3,22 @@ require('words')
 require('definitions')
 
 describe('Word') do
+  before() do
+    Word.clear()
+  end
+
   describe('#initialize') do
-    it "initializes a new Word object" do
+    it "initialize a new Word object" do
       test_word = Word.new('name')
       expect(test_word.word()).to(eq('name'))
+    end
+  end
+
+  describe(':save') do
+    it "save a Word object into @@words class variable" do
+      test_word = Word.new('name')
+      test_word.save()
+      expect(Word.all()).to(eq([test_word]))
     end
   end
 end
